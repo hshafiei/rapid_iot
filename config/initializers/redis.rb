3921128be825f90d3592d5e9b468765e1c76b3b3
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-08-06 11:10:51 +0430

    redis.rb changed

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-if ENV["REDISCLOUD_URL"]
+if !ENV["REDISCLOUD_URL"].blank?
     $redis = Resque.redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 else
   $redis = Redis::Namespace.new("redis_with_rails", :redis => Redis.new)
